024f1b125f610c36ec60e79c3e05e7c629ae9a75
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2019-01-14 13:16:10 +0530

    Get game working without any edge conditions.
    Some functionalities are hardcoded. Bricks creation is hardcoded.

--- controller.js
+++ controller.js
@@ -6,37 +6,107 @@ const movePaddle = function(document, paddle) {
   if (event.key == "ArrowRight") {
     paddle.moveRight();
   }
-  if (event.key == "ArrowLeft") {
+  if (event.key == "ArrowLeft" && paddle.left > 0) {
     paddle.moveLeft();
   }
   drawPaddle(document, paddle);
 };
 
-const drawPaddle = function(document, paddle) {
+const moveBall = function(document, game) {
+  setInterval(() => {
+    game.walls.isBallCollide(game.ball);
+    game.roofAndGround.isBallCollide(game.ball);
+    game.ball.move();
+    game.paddle.isBallCollide(game.ball);
+    game.paddle.isPaddleAtBorder(game.walls.width);
+    drawBall(document, game.ball);
+    game.checkCollisionBrick();
+    destroyBrick(document, game);
+  }, 50);
+};
+
+const drawPaddle = function(document, paddle, ball) {
   let paddleDiv = document.getElementById("paddle_1");
 
   paddleDiv.style.width = addPixelSuffix(paddle.width);
   paddleDiv.style.height = addPixelSuffix(paddle.height);
   paddleDiv.style.bottom = addPixelSuffix(paddle.bottom);
   paddleDiv.style.left = addPixelSuffix(paddle.left);
+};
+
+const drawBall = function(document, ball) {
+  let ballDiv = document.getElementById("ball_1");
+
+  ballDiv.style.width = addPixelSuffix(ball.radius);
+  ballDiv.style.height = addPixelSuffix(ball.radius);
+  ballDiv.style.bottom = addPixelSuffix(ball.bottom);
+  ballDiv.style.left = addPixelSuffix(ball.left);
+};
+
+const drawBricks = function(bricks) {
   let screen = getScreen(document);
+
+  for (let counter = 0; counter < bricks.length; counter++) {
+    let brick = document.createElement("div");
+    brick.className = "brick";
+    brick.id = bricks[counter].id;
+    brick.style.width = addPixelSuffix(bricks[counter].width);
+    brick.style.height = addPixelSuffix(bricks[counter].height);
+    brick.style.left = addPixelSuffix(bricks[counter].left);
+    brick.style.bottom = addPixelSuffix(bricks[counter].bottom);
+    screen.appendChild(brick);
+  }
+};
+
+const initializePaddle = function(document, paddle) {
+  let screen = getScreen(document);
+
+  let paddleDiv = document.createElement("div");
+  paddleDiv.className = "paddle";
+  paddleDiv.id = "paddle_1";
+  screen.appendChild(paddleDiv);
+
   screen.tabIndex = "0";
   screen.focus();
   screen.onkeydown = movePaddle.bind(null, document, paddle);
 };
 
-const initializePaddle = function(document) {
+const destroyBrick = function(document, game) {
+  const bricksToDstroy = game.checkCollisionBrick();
+  bricksToDstroy.map(brick => {
+    const child = document.getElementById(brick.id);
+    child.remove();
+    game.ball.dy = -game.ball.dy;
+  });
+};
+
+const initilizeBall = function(document, ball) {
   let screen = getScreen(document);
-  let paddleDiv = document.createElement("div");
-  paddleDiv.className = "paddle";
-  paddleDiv.id = "paddle_1";
-  screen.appendChild(paddleDiv);
+  let ballDiv = document.createElement("div");
+  ballDiv.className = "ball";
+  ballDiv.id = "ball_1";
+  screen.appendChild(ballDiv);
 };
 
 const initilize = document => {
-  let paddle = new Paddle(100, 20, 430, 5);
-  initializePaddle(document);
+  let paddle = new Paddle(100, 20, 430, 5, 20);
+  let ball = new Ball(30, 465, 25, 10, 10);
+  let walls = new Walls(960);
+  let roofAndGround = new RoofAndGround(700);
+  let bricks = new Bricks(960, 700);
+  let game = new Game(
+    ball,
+    paddle,
+    walls,
+    roofAndGround,
+    bricks.createBricks()
+  );
+  drawBricks(bricks.createBricks());
+  initializePaddle(document, paddle);
+  initilizeBall(document, ball);
   drawPaddle(document, paddle);
+  drawBall(document, ball);
+  moveBall(document, game);
 };
 
 window.onload = initilize.bind(null, document);
--- model.js
+++ model.js
@@ -1,23 +1,140 @@
+const isInBetween = function(upper, lower, number) {
+  return number > lower && number < upper;
+};
+
 class Paddle {
-  constructor(width, height, left, bottom) {
+  constructor(width, height, left, bottom, speed) {
     this.width = width;
     this.height = height;
     this.left = left;
     this.bottom = bottom;
+    this.speed = speed;
   }
   moveLeft() {
-    this.left -= 10;
+    this.left -= this.speed;
   }
   moveRight() {
-    this.left += 10;
+    this.left += this.speed;
+  }
+
+  isBallCollide(ball) {
+    let isBallOnPaddle = isInBetween(
+      this.left + this.width,
+      this.left,
+      ball.left
+    );
+    if (ball.bottom == this.bottom + this.height && isBallOnPaddle) {
+      ball.dy = -1 * ball.dy;
+      ball.dx = ball.dx;
+    }
+  }
+
+  isPaddleAtBorder(screenWidth) {
+    if (this.left + this.width + this.speed >= screenWidth) {
+      this.left = screenWidth - (this.width + this.speed);
+    }
+    if (this.left <= 0) {
+      this.left = 0;
+    }
   }
 }
 
-class Screen {
+class Ball {
+  constructor(radius, left, bottom, dx, dy) {
+    this.radius = radius;
+    this.left = left;
+    this.bottom = bottom;
+    this.dx = dx;
+    this.dy = dy;
+  }
+  move() {
+    this.left = this.left + this.dx;
+    this.bottom = this.bottom + this.dy;
+  }
+}
+
+class Walls {
+  constructor(width) {
+    this.width = width;
+  }
+  isBallCollide(ball) {
+    if (!(ball.left + ball.radius + ball.dx <= this.width && ball.left >= 0)) {
+      ball.dx = -1 * ball.dx;
+      ball.dy = ball.dy;
+    }
+  }
+}
+
+class RoofAndGround {
+  constructor(height) {
+    this.height = height;
+  }
+
+  isBallCollide(ball) {
+    if (ball.bottom + ball.radius + ball.dy >= this.height) {
+      ball.dy = -1 * ball.dy;
+      ball.dx = ball.dx;
+    }
+    if (ball.bottom <= 0) {
+      ball.dy = 0;
+      ball.dx = 0;
+    }
+  }
+}
+
+class Brick {
+  constructor(width, height, left, bottom, id) {
+    this.width = width;
+    this.height = height;
+    this.left = left;
+    this.bottom = bottom;
+    this.id = id;
+  }
+}
+
+class Bricks {
   constructor(width, height) {
     this.width = width;
     this.height = height;
   }
+
+  createBricks() {
+    let bricks = [];
+    let brickWidth = 90;
+    let left = 5;
+    let bottom = this.height - 15;
+    for (let rows = 1; rows < 10; rows++) {
+      for (let column = 1; column < 11; column++) {
+        let brickId = `brick_${column}_${rows}`;
+        let brick = new Brick(brickWidth, 15, left, bottom, brickId);
+        bricks.push(brick);
+        left = left + brickWidth + 5;
+      }
+      left = 5;
+      bottom = bottom - 20;
+    }
+    return bricks;
+  }
 }
 
-class Game {}
+const checkCollisionWithBrick = function(ball, brick) {
+  return (
+    isInBetween(brick.left + brick.width, brick.left, ball.left) &&
+    isInBetween(brick.bottom, brick.bottom - ball.radius, ball.bottom)
+  );
+};
+
+class Game {
+  constructor(ball, paddle, walls, roofAndGround, bricks) {
+    this.ball = ball;
+    this.paddle = paddle;
+    this.walls = walls;
+    this.roofAndGround = roofAndGround;
+    this.bricks = bricks;
+  }
+
+  checkCollisionBrick() {
+    const hasTouchedBrick = checkCollisionWithBrick.bind(null, this.ball);
+    return this.bricks.filter(hasTouchedBrick);
+  }
+}
